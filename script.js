37a0d39060ce113ef6b7d81caad5828ebea253da
Shrihari Kale <163184413+shrihari-kale@users.noreply.github.com>
2026-02-24 22:56:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 function firstChar(text) {
-
-  // your code here
-	 for(let i = 0; i < text.length; i++){
-		 if (text[i] !== " "){
-			 return text[i];
-		 }
-	 }
-return " ";
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] !== " ") {
+      return text[i];
+    }
+  }
+  return "";
 }
 
-// Do not change the code below
-//Uncomment the following line to show the prompt popup
 const text = prompt("Enter text:");
 alert(firstChar(text));
