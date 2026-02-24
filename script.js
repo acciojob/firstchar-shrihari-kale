dc324f609477ea6e3aca04ea02165b326c195091
Shrihari Kale <163184413+shrihari-kale@users.noreply.github.com>
2026-02-24 22:48:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function firstChar(text) {
+
   // your code here
 	 for(let i = 0; i < text.length; i++){
-		 if(text[i] !== ""){
+		 if(text[i] !== " "){
 			 return text[i];
 		 }
 	 }
-return "";
+return " ";
+}
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
